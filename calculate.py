ead39ccef1e68e0643f9b297caa85144bcb0a6da
Brett Henderson <bhenders@login1.int.omega.computecanada.dev>
2024-12-02 02:29:00 +0000

    updated

--- calculate.py
+++ calculate.py
@@ -10,6 +10,8 @@ parser.add_argument('zero_field', help='The file containing the zero field polar
 parser.add_argument('relaxed_ion', help='The file or files containing the relaxed ion polarization output.', nargs='+')
 parser.add_argument('--clamped-ion', '-c', help='The file containing the clamped ion polarization output.')
 parser.add_argument('--plot', '-p', action='store_true', help='Plot the extracted polarization curve to check it.')
+parser.add_argument('--extend', '-e', action='store_true', help='keep appending steps rather than overwriting them for step 9 (if reset_counters used by accident).')
+parser.add_argument('--nosort', '-n', action='store_true', help='do not sort filenames. Use the input order.')
 parser.add_argument('--efield', type=float, default=0.001, help='Electric field in au, default=0.001.')
 parser.add_argument('--eps-bulk', type=float, default=9.26, help='Rel Permittivity of the bulk, for calculating enhancement, default=9.26.')
 parser.add_argument('--eps-inf-bulk', type=float, default=3.04, help='High frequency Rel Permittivity of the bulk, default=3.04')
@@ -35,15 +37,30 @@ if args.clamped_ion:
 
 # Get the relaxed ion polarization from all steps
 files = args.relaxed_ion
-files.sort()
-pols = {}
-for file in files:
-    _, pol = parsers.get_dipole(file)
-    if len(pol):
-        for i, p in enumerate(pol):
-            pols[pol[i, 0]] = pol[i]
-p2 = OrderedDict(sorted(pols.items()))
-p2 = np.concatenate([pols[step].reshape(1,-1) for step in pols], axis=0)
+if not args.nosort:
+    files.sort()
+
+if not args.extend:
+    pols = {}
+    for file in files:
+        print(file)
+        _, pol = parsers.get_dipole(file)
+        if len(pol):
+            for i, p in enumerate(pol):
+                pols[pol[i, 0]] = pol[i]
+    p2 = OrderedDict(sorted(pols.items()))
+    p2 = np.concatenate([pols[step].reshape(1,-1) for step in pols], axis=0)
+else:
+    p2 = None
+    for file in files:
+        print(file)
+        _, pol = parsers.get_dipole(file)
+        if len(pol):
+            if p2 is None:
+                p2 = pol
+            else:
+                pol[:, 0] += p2[-1, 0]
+                p2 = np.concatenate([p2, pol], axis=0)
 
 # sum up cumulatively over timesteps and make sure end of step 8 aligns with beginning of 9
 if args.clamped_ion:
@@ -69,45 +86,50 @@ ae = analysis.correct_jumps(ae_uncorrected, jump_quantum=pquant, jump_threshold=
 ai = analysis.correct_jumps(ai_uncorrected, jump_quantum=pquant)
 
 coords = parsers.get_final_positions(args.zero_field)
-al = len(coords[args.inclusion_element])/(sum([len(coords[el]) for el in coords]))
+try:
+    al = len(coords[args.inclusion_element])/(sum([len(coords[el]) for el in coords]))
+except:
+    al = 1
 eps_r = 1 + 4*np.pi * (atot[-1]) / vol / args.efield               # atot already is referenced to initial pol
 eps_inf = 1 + 4*np.pi * (p1[-1, -1] - p0[-1]) / vol / args.efield  # assumes no jumps in clamped ion step
 alpha_r = (eps_r - args.eps_bulk) / (4 * np.pi * al)
 alpha_inf = (eps_inf - args.eps_inf_bulk) / (4 * np.pi * al)
 
+print(f'pq = {pquant}, p_i = {p0[-1]}, p_ci = {p1[-1, -1]}, p_f = {p2[-1, -1]}, vol={vol}, efield = {args.efield}')
 print(
 f"""
 Dielectric Constants:
 
-|  High Frequency  |  Low Frequency  |  alpha_inf  |   alpha_r  | 
-|------------------|-----------------|-------------|------------|
-|        {eps_inf:.2f}      |       {eps_r:.2f}     |    {alpha_inf:.2f}     |     {alpha_r:.2f}   | 
+|  High Frequency   |  Low Frequency   |  alpha_inf   |   alpha_r   | 
+|-------------------|------------------|--------------|-------------|
+|        {eps_inf:.3f}      |       {eps_r:.3f}     |    {alpha_inf:.3f}     |     {alpha_r:.3f}   | 
 
 * alpha values calculated using a bulk matrix relative permittivity of {args.eps_bulk} 
   and high frequency permittivity {args.eps_inf_bulk}.
 """
 )
 
-# plot to check that polarization quantum jumps have been smoothed
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,6))
+if args.plot:
+    # plot to check that polarization quantum jumps have been smoothed
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,6))
 
-# uncorrected
-ax1.set_title('Uncorrected Cell Dipole', fontsize=18)
-ax1.plot(time, atot_uncorrected, label="Total")
-ax1.plot(time, ae_uncorrected, label="Electronic")
-ax1.plot(time, ai_uncorrected, label="Ionic")
-ax1.legend(fontsize=16)
-ax1.set_xlabel('Time / au', fontsize=16)
-ax1.set_ylabel('Cell Dipole', fontsize=16)
-ax1.hlines(0, 0,6400, 'k', linestyle='--')
+    # uncorrected
+    ax1.set_title('Uncorrected Cell Dipole', fontsize=18)
+    ax1.plot(time, atot_uncorrected, label="Total")
+    ax1.plot(time, ae_uncorrected, label="Electronic")
+    ax1.plot(time, ai_uncorrected, label="Ionic")
+    ax1.legend(fontsize=16)
+    ax1.set_xlabel('Time / au', fontsize=16)
+    ax1.set_ylabel('Cell Dipole', fontsize=16)
+    ax1.hlines(0, 0,6400, 'k', linestyle='--')
 
-# corrected
-ax2.set_title('Corrected Cell Dipole', fontsize=18)
-ax2.plot(time, atot, label='Total')
-ax2.plot(time, ae, label='Electronic')
-ax2.plot(time, ai, label='Ionic')
-ax2.hlines(0, 0,6400, 'k', linestyle='--')
-ax2.legend()
+    # corrected
+    ax2.set_title('Corrected Cell Dipole', fontsize=18)
+    ax2.plot(time, atot, label='Total')
+    ax2.plot(time, ae, label='Electronic')
+    ax2.plot(time, ai, label='Ionic')
+    ax2.hlines(0, 0,6400, 'k', linestyle='--')
+    ax2.legend()
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
